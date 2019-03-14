1c0f5557c5bd6af685fe5b3902e3d918ab2cc7cc
tuzmusic <tuzmusic@gmail.com>
2019-03-14 12:56:16 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,8 +4,12 @@ export default function shoppingListItemReducer(
 	},
 	action
 ) {
+  console.log(action);
+  
 	switch (action.type) {
-		case 'INCREASE_COUNT':
+    case 'INCREASE_COUNT':
+      console.log('Current state.items length %s', state.items.length);
+      console.log('Updating state.items length to %s', state.items.length + 1);
 			return state.items.concat(state.items.length + 1);
 
 		default:
