cf277713683c277a8beff19fce9ce47875f9bc42
github username <github email address>
2019-07-06 12:29:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
+import { createStore } from 'redux'; /* import the createStore method from the redux library...no more writing it by hand! */
+import {Provider} from 'react-redux' /* import the Provider component from the react-redux library. The Provider wraps the top-level component --<App /> in this case (see below) -- and allows for the store to be passed in only once. no more prop hell!*/
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//this code allows the provider to access store. the var calls createStore (a redux method) and passes in our reducer. the two window references 1) look for a method called __REDUX_DEVTOOLS_EXTENSION__ and if there, it 2) executes the method
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,30 @@
+//this reducer is imported into our index.js file so when createStore calls the dispatch method, it can pass the reducer -- in this case one that increases the number of items on a list -- which can take an action and update the state
+
 export default function shoppingListItemReducer(
 	state = {
 		items: []
 	},
 	action
 ) {
+	//add this check and the logs in the switch so that we can debug our action and state
+	console.log(action);
+
 	switch (action.type) {
+		// this case adds a new item to a list and that number is 1 more than the previous item
 		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+			console.log('Current state.items length %s', state.items.length);
+      console.log('Updating state.items length to %s',  state.items.length + 1);
+
+			//added the code below to check our shopping list items
+			return Object.assign({}, state, {
+        items: state.items.concat(state.items.length + 1)
+      });
+
 
 		default:
+			console.log('Initial state.items length: %s', state.items.length)
+
 			return state;
+
 	}
 }
