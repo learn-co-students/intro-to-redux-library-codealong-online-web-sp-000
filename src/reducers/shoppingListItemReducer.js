f5fd8e1dfbce2c43596b98f0c3c61a04ccedbb08
abogailo <abagailanne@gmail.com>
2020-02-08 13:02:55 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,18 @@
 export default function shoppingListItemReducer(
 	state = {
-		items: []
+	  items: []
 	},
 	action
-) {
+  ) {
 	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
-		default:
-			return state;
+	  case 'INCREASE_COUNT':
+		return {
+		  ...state,
+		  items: state.items.concat(state.items.length + 1)
+		}
+   
+	  default:
+		console.log('Initial state.items length: %s', state.items.length);
+      return state;
 	}
-}
+  }
