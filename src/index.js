d988df17ef8d0d1363ad8fd3c4297b76c0e1b747
treuther <tracyreuther@gmail.com>
2020-02-18 18:15:33 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'; /* code change */
+import { Provider } from 'react-redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(shoppingListItemReducer,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+//code change - added Provider to wrap around the app
+//Provider wraps the top level component (App, in this case) and is the only component where store is passed in.
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+document.getElementById('root')
+);
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,13 +4,17 @@ export default function shoppingListItemReducer(
 	},
 	action
 ) {
+	console.log(action); //logging the action
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length); //storing the state as current state
+			console.log('Updating state.items length to %s', state.items.length + 1); //logging the updated state value
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log('Initial state.items length: %s', state.items.length); //log the previous state because this state is unchanged
 			return state;
 	}
 }
