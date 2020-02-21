5537e64ea49e4edee28426229372fa5ca727e374
jgrantps <jgrantps@hotmail.com>
2020-02-21 15:38:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-// ./src/index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
-import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+ 
+class App extends Component {
+  handleOnClick = event => {
+    this.props.increaseCount();
+  };
+ 
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleOnClick}>Click</button>
+        <p>{this.props.items.length}</p>
+      </div>
+    );
+  }
+}
+ 
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  };
+};
+ 
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
+  };
+};
+ 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
