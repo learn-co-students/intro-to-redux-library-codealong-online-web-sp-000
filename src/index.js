a38d568b45304afcba537a1c7d53a06b186c9100
ch2isk4kos <ch2isk4kos@gmail.com>
2020-03-15 22:36:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'; /* code change */
+// import { createStore, applyMiddleware } from 'redux'; /* code change */
+// import { composeWithDevTools } from 'redux-devtools-extension'; /* code change */
+import { Provider } from 'react-redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(shoppingListItemReducer);
+
+const store = createStore(
+    shoppingListItemReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// or
+
+// const store = createStore(
+//     shoppingListItemReducer,
+//     composeWithDevTools(applyMiddleware(...middleware),
+//     // other store enhancers if any
+// ));
+
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// code change - added Provider to wrap around App
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+// By including the Provider, you'll be able to access the Redux store
+// and/or dispatch actions from any component you want,
+// regardless of where it is on the component tree.
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,45 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
-		default:
-			return state;
-	}
+// export default function shoppingListItemReducer(state = {items: []}, action) {
+// 	switch (action.type) {
+// 	case 'INCREASE_COUNT':
+// 		return {
+// 		...state,
+// 		items: state.items.concat(state.items.length + 1)
+// 		}
+// 	default:
+// 		return state;
+// 	}
+// }
+
+export default function shoppingListItemReducer(state = {items: []}, action) {
+  	console.log(action);
+
+  	switch (action.type) {
+	case 'INCREASE_COUNT':
+      	console.log('Current state.items length %s', state.items.length);
+      	console.log('Updating state.items length to %s', state.items.length + 1);
+
+		return {
+        	...state,
+        	items: state.items.concat(state.items.length + 1)
+      	};
+
+    default:
+      	console.log('Initial state.items length: %s', state.items.length);
+      	return state;
+  	}
 }
+
+// on line 14: you'e logging the action.
+
+// on line 22 - after the case statement -
+// you're storing state as current state first,
+// then logging the updating state value.
+
+// on line 27 - after the default case statement -
+// you can log the previous state because this state is unchanged.
+
+// while you aren't getting your state directly from the store,
+// you're still dispatching actions.
+
+// you know this to be true b/c each time you click a button, you're calling store.dispatch({ type: 'INCREASE_COUNT' })
+// and somehow this is hitting the reducer. So things are happening.
