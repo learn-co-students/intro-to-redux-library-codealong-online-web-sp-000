b4bb7236d968296cbc6ec287b9093af1e90a5463
github username <github email address>
2020-05-26 19:09:44 -0400

    Trying manually

--- src/index.js
+++ src/index.js
@@ -8,11 +8,18 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(
-  shoppingListItemReducer, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
-);
+// const store = createStore(
+//   shoppingListItemReducer, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+// );
 //code change - added Provider to wrap around App
 //ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+   reducer, /* preloadedState, */
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
+
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
