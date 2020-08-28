b2ed2b505f5156c158fd6f037d87ff2432c34950
Soner Mezgitci <mezgitci9@gmail.com>
2020-08-27 21:18:33 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,23 @@
 export default function shoppingListItemReducer(
 	state = {
-		items: []
+	  items: []
 	},
 	action
-) {
+  ) {
+	console.log(action);
 	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
-		default:
+	  case 'INCREASE_COUNT':
+		console.log('Current state.items length %s', state.items.length);
+		console.log('Updating state.items length to %s', state.items.length + 1);
+		return {
+		  ...state,
+		  items: state.items.concat(state.items.length + 1)
+		//The concat() method is used to merge two or more arrays. 
+		//This method does not change the existing arrays, but instead returns a new array.
+      };
+ 
+	  default:
+			console.log('Initial state.items length: %s', state.items.length);
 			return state;
-	}
-}
+		}
+	  }
