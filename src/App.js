c1f50e6a86442e839b19c89762984bd3d1be8852
David Strauch <dmaster18@gmail.com>
2020-12-25 18:33:52 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -33,3 +33,30 @@ export default connect(
 	mapStateToProps,
 	mapDispatchToProps
 )(App);
+
+	render() {
+		return (
+			<div className="App">
+				<button onClick={this.handleOnClick}>Click</button>
+				<p>{this.props.items.length}</p>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		items: state.items
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
